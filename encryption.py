2ab0731ad69d94bbab968473c69d28095c8af53b
Tzon Kennenty <tzonkennenty@Tzons-Laptop.local>
2025-04-02 14:34:07 +0300

    Move all files to root directory

---
+++ encryption.py
@@ -0,0 +1,24 @@
+
+from cryptography.fernet import Fernet
+import os
+
+KEY_FILE = "secret.key"
+
+def load_key():
+    if not os.path.exists(KEY_FILE):
+        key = Fernet.generate_key()
+        with open(KEY_FILE, "wb") as key_file:
+            key_file.write(key)
+    else:
+        with open(KEY_FILE, "rb") as key_file:
+            key = key_file.read()
+    return key
+
+key = load_key()
+fernet = Fernet(key)
+
+def encrypt(data: str) -> str:
+    return fernet.encrypt(data.encode()).decode()
+
+def decrypt(token: str) -> str:
+    return fernet.decrypt(token.encode()).decode()
