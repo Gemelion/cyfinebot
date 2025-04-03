a1be0bd1d10992dd2659ed1f777fb845eb31631e
Tzon Kennenty <tzonkennenty@Tzons-Laptop.local>
2025-04-03 12:25:08 +0300

    Fix ImportError: added proper exports to handlers/__init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,4 @@
-from .registration import register_registration_handlers
-from .menu import register_menu_handlers
-from .admin import register_admin_handlers
-from .privacy import register_privacy_handlers
-
-def register_all_handlers(app):
-    register_registration_handlers(app)
-    register_menu_handlers(app)
-    register_admin_handlers(app)
-    register_privacy_handlers(app)
+from .start_handler import start_handler
+from .fine_check_handler import fine_check_handler
+from .gdpr_handler import gdpr_handler
+from .admin_handler import admin_handler
