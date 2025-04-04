718f3beca68888b06faa394a1daca12efc5e9d67
Tzon Kennenty <tzonkennenty@Tzons-Laptop.local>
2025-04-04 12:38:13 +0300

    fix: bot_webhook с поддержкой переменных окружения

--- bot_webhook.py
+++ bot_webhook.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI, Request
-from telegram import Update
 from bot import get_bot_application
+from telegram import Update
 import os
 
+bot_app = get_bot_application()
 app = FastAPI()
-
-bot_app = get_bot_application(os.getenv("BOT_TOKEN"))
-
-@app.on_event("startup")
-async def startup_event():
-    await bot_app.initialize()
 
 @app.post("/webhook/{token}")
 async def telegram_webhook(token: str, request: Request):
-    data = await request.json()
-    update = Update.de_json(data, bot_app.bot)
+    if token != os.getenv("BOT_TOKEN"):
+        return {"status": "unauthorized"}
+    update = Update.de_json(await request.json(), bot_app.bot)
     await bot_app.process_update(update)
-    return {"ok": True}
+    return {"status": "ok"}
