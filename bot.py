4fab98d96352059f25164080ba523d66f24c5d27
Tzon Kennenty <tzonkennenty@Tzons-Laptop.local>
2025-04-04 12:54:12 +0300

    fix: исправлен импорт init_db из database

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
-import os
+from database import init_db
 from telegram.ext import ApplicationBuilder
-from handlers.start_handler import start_handler
-from db import init_db
 
+from handlers.start_handler import start_handler
 
 def get_bot_application():
-    # Инициализация базы данных при запуске бота
-    init_db()
-
     application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
     application.add_handler(start_handler)
-    application.initialize()  # Инициализация Application вручную
+    init_db()
     return application
