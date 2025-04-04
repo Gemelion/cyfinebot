fbe82182049893f5b473519dcf20fdef95c09bbc
Tzon Kennenty <tzonkennenty@Tzons-Laptop.local>
2025-04-04 11:22:49 +0300

    Init DB and setup start_handler in bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
-from telegram.ext import (
-    Application,
-    ApplicationBuilder,
-)
-from handlers.start_handler import start_handler
 import os
+from telegram.ext import Application
+from handlers.start_handler import start_handler
+from db import init_db
 
 def get_bot_application(token: str) -> Application:
-    application = ApplicationBuilder().token(token).build()
+    application = Application.builder().token(token).build()
     application.add_handler(start_handler)
     return application
+
+# При импорте и запуске — инициализируем базу
+init_db()
