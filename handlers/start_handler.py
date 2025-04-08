fa60fb9f99aec93a1418bc0624cffc4ad5335144
Tzon Kennenty <tzonkennenty@Tzons-Laptop.local>
2025-04-08 21:20:11 +0300

    Fix: update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -6,6 +6,6 @@ from aiogram import F
 
 router = Router()
 
-@router.message(Command("start"))
+@router.message(F.command("start"))
 async def cmd_start(message: Message):
     await message.answer("Привет! Чем могу помочь?")
